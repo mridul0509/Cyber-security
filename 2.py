672624742d8c2ab0e0c568b2508921df0b65fd94
Mridul Mishra <60619014+mridul0509@users.noreply.github.com>
2020-08-29 15:11:50 +0530

    Delete 2.py

--- 2.py
+++
@@ -1,9 +0,0 @@
-low=int(input("enter low value "))
-high=int(input("enter high value "))
-def check(low,high):
- n=float(input("Enter number to be checked "))
- if(n>=low and n<=high):
-  print("In range")
- else:
-  print("Not in range")
-check(low,high)
