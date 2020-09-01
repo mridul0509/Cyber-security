3fb6dfb7e06f1355c7b1fbda21c3ffe6781a9973
mridul0509 <mridulmishra0812@gmail.com>
2020-09-01 15:32:26 +0530

    Assignment 2

---
+++ Assignment2/8.py
@@ -0,0 +1,14 @@
+r=int(input("Enter range "))
+def prime(j):
+	c=0
+	p="Prime"
+	np="Nonprime"
+	for k in range(2,j):
+		if(j%k==0):
+			c=c+1
+	if(c==0):
+		return p
+	else:
+		return np
+l={i:prime(i) for i in range(2,r+1)}
+print(l)
